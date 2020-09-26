043cdb913622795053992dab0e4ec4908744aa42
Daniel Binoy <dbinoy15@gmail.com>
2020-09-26 16:56:47 -0700

    setup

---
+++ src/components/MazeWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MazeWrapper.css';
+
+function MazeWrapper(){
+    return (
+        <div className = "maze-wrapper">
+            <h1 className = "main-header">This is  a test</h1>
+
+        </div>
+
+    );
+}
+
+export default MazeWrapper;
