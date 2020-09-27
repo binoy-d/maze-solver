36423322e5847c0e56793ecab9a5eeaea02b965f
Daniel Binoy <dbinoy15@gmail.com>
2020-09-26 17:03:33 -0700

    create arr of tiles

--- src/components/MazeWrapper.js
+++ src/components/MazeWrapper.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './MazeWrapper.css';
-
+import Tile from './Tile'
 function MazeWrapper(){
+
+    var maze = [];
+
+    for(let i = 0; i<25; i++){
+        
+        maze.push(<Tile size={20}/>)
+    }
     return (
         <div className = "maze-wrapper">
             <h1 className = "main-header">This is  a test</h1>
+            {maze.map((tile, index)=>
+            tile
+            )}
 
+            
         </div>
 
     );
---
+++ src/components/Tile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Tile.css';
+
+function Tile({size}){
+    return (
+        <div style = {{width:size, height:size}} className = "tile-wrapper">
+            
+        </div>
+
+    );
+}
+
+export default Tile;
